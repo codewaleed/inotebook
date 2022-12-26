d1e64d047f93fd1ded0c59fc95b824882d02febc
ossols <waleednasir.ossols@gmail.com>
2022-12-26 06:27:16 +0500

    v 51

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,54 +1,100 @@
-const { Router } = require("express");
 const express = require("express");
-const Users = require("../models/Users");
+const User = require("../models/User");
 const router = express.Router();
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
 const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+var jwt = require("jsonwebtoken");
 
-const JWT_SECRET = 'mynameiskhanbitch1'
+const JWT_SECRET = "Harryisagoodb$oy";
 
-//done
-//mongodb+srv://wali2:wali2@cluster0.boucay5.mongodb.net/test
-
-//create a user using POST: /api/auth/createuser "no login required"
+//Route 1: Create a User using: POST "/api/auth/createuser". No login required
 router.post(
   "/createuser",
   [
-    body("name", "enter valid name").isLength({ min: 3 }),
-    body("email", "enter valid email").isEmail(),
-    body("password").isLength({ min: 5 }),
+    body("name", "Enter a valid name").isLength({ min: 3 }),
+    body("email", "Enter a valid email").isEmail(),
+    body("password", "Password must be atleast 5 characters").isLength({
+      min: 5,
+    }),
+  ],
+  async (req, res) => {
+    // If there are errors, return Bad request and the errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+      // Check whether the user with this email exists already
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ error: "Sorry a user with this email already exists" });
+      }
+      const salt = await bcrypt.genSalt(10);
+      const secPass = await bcrypt.hash(req.body.password, salt);
+
+      // Create a new user
+      user = await User.create({
+        name: req.body.name,
+        password: secPass,
+        email: req.body.email,
+      });
+      const data = {
+        user: { 
+          id: user.id,
+        },
+      };
+      const authtoken = jwt.sign(data, JWT_SECRET);
+
+      // res.json(user)
+      res.json({ authtoken });
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Some Error occured");
+    }
+  }
+);
+
+//Route 2: Create a User using: POST "/api/auth/login".
+router.post(
+  "/login",
+  [
+    body("email", "Enter a valid email").isEmail(),
+    body("password", "Password cannot be blank").exists()
   ],
   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-
-    const salt = await bcrypt.genSalt(10);
-    const secPass = await bcrypt.hash(req.body.password, salt);
-
-    Users.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: secPass,
-    })
-
-    const data={
-      user:{
-        id: user.id
+    const {email, password} = req.body;
+    try{
+      let user = await User.findOne({email})
+      if(!user){
+        return res.status(400).json({error:"try to login with correct credentials"})
       }
+      const passwordCompare = await bcrypt.compare(password, user.password)
+
+      if(!passwordCompare){
+        return res.status(400).json({error:"try to login with correct credentials"})
+      }
+      const data ={
+        user: {
+          id: user.id
+        }
+        
+      }
+      const authtoken = jwt.sign(data, JWT_SECRET);
+
+      // res.json(user)
+      res.json({ authtoken });
     }
+    catch(error){
+      console.error(error.message);
+      res.status(500).send("Internal Error occured");
+    }
+  })
 
-    //const jwtToken = jwt.sign(data, JWT_SECRET);
-    //console.log(jwtToken)
-
-      .then((user) => res.json(user))
-      .catch((err) => {
-        console.log(err);
-        res.json({ error: "dont duplicate values" });
-      });
-  }
-);
 
 module.exports = router;
