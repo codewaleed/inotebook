2448118552fd57e64426316fd3bd12be12b33291
ossols <waleednasir.ossols@gmail.com>
2023-02-13 10:24:23 +0500

    video 52

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,12 +1,65 @@
 const { Router } = require("express");
 const express = require("express");
+const Note = require("../models/Note");
+const { body, validationResult } = require("express-validator");
 const router = express.Router();
+var fetchuser = require("../middleware/fetchuser");
 
-router.get("/", (req, res) => {
-  obj = {
-    name: "dani",
-  };
-  res.json(obj);
+//Route 1: get All the Notes using: GET "/api/notes/fetchallnotes". login required
+router.get("/fetchallnotes", fetchuser, async (req, res) => {
+  try {
+    const notes = await Note.find({ user: req.user.id });
+    res.json(notes);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Error occured");
+  }
 });
+
+//Route 2: Add a new Note using: POST "/api/notes/addnote". login required
+router.post(
+  "/addnote",
+  fetchuser,
+  [
+    body("title", "Enter a valid title").isLength({ min: 3 }),
+    body("description", "desc must be atleast 5 characters").isLength({
+      min: 5,
+    }),
+  ],
+  async (req, res) => {
+    try {
+      const { title, description, tag } = req.body;
+
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+      const note = new Note({
+        title,
+        description,
+        tag,
+        user: req.user.id,
+      });
+      const savedNote = await note.save();
+      res.json(savedNote);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Internal Error occured: add note");
+    }
+  }
+);
+
+//Route 3: update Note using: PUT "/api/notes/updatenote". login required
+router.put(
+  "/updatenote/:id",
+  fetchuser,
+  [
+    body("title", "Enter a valid title").isLength({ min: 3 }),
+    body("description", "desc must be atleast 5 characters").isLength({
+      min: 5,
+    }),
+  ],
+  async (req, res) => {}
+);
 
 module.exports = router;
