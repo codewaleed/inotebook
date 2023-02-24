0e4ae25f492cfaabc339ae99127a4d2a7eec6625
ossols <waleednasir.ossols@gmail.com>
2023-02-24 10:59:55 +0500

    video 65

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
-const mongoURI = "mongodb+srv://wali2:wali2@cluster0.boucay5.mongodb.net/inotebook";
-
+const mongoURI =
+  //"mongodb+srv://wali2:wali2@cluster0.boucay5.mongodb.net/inotebook";
+  "mongodb://wali2:wali2@ac-xhaqp3d-shard-00-00.boucay5.mongodb.net:27017,ac-xhaqp3d-shard-00-01.boucay5.mongodb.net:27017,ac-xhaqp3d-shard-00-02.boucay5.mongodb.net:27017/?ssl=true&replicaSet=atlas-11thvo-shard-0&authSource=admin&retryWrites=true&w=majority";
 const connectToMongo = () => {
   mongoose.connect(mongoURI, () => {
     console.log("connected mongodb");
